98a1b3942b88f79e2f36c8a1c45775f1120a9cc9
iosfeng <iosfeng@163.com>
2016-10-31 00:32:49 +0800

    优化NSError处理器

--- FYHelper/FYHelper.h
+++ FYHelper/FYHelper.h
@@ -9,39 +9,26 @@
 #ifndef FYHelper_h
 #define FYHelper_h
 
-
-#import "FYDeviceUtil.h"
+// category
 #import "FYCategory.h"
+
+// subclass
 #import "FYPaddingTextField.h"
+
+// view controller
 #import "FYViewController.h"
 #import "FYNavigationController.h"
+
+// macro definition
 #import "FYMacroDefinition.h"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// utils
+#import "FYAppUtil.h"
+#import "FYDeviceUtil.h"
+#import "FYErrorUtil.h"
+#import "FYFileUtil.h"
+#import "FYLocationUtil.h"
+#import "FYOtherUtil.h"
 
 
 #endif /* FYHelper_h */
